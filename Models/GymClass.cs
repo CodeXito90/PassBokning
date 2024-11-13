40edbe9278049b08870be2994a75420b380ddd72
jama mohamed abdi <j.m.abdi@hotmail.com>
2024-11-13 10:01:17 +0100

    Add project files.

---
+++ Models/GymClass.cs
@@ -0,0 +1,12 @@
+﻿namespace PassBokning.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        DateTime StartTime { get; set; }
+        TimeSpan Duration { get; set; }
+        DateTime EndTime { get {  return StartTime + Duration; } }
+        public string Description { get; set; }
+    }
+}
